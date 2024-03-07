cd93c4dbaad0e2eb14ad1e20d4b7a3ef20a06eef
PeepoGaem <107309688+PeepoGaem@users.noreply.github.com>
2024-03-07 10:16:05 +0100

    Drone + Tex + Mat
    - Drone Model, Material and propeller script added
    - Added materials for hedge and terrain floor.

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float rotationSpeed = 50f; // Configurable speed
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the object around the Y-axis based on the configured speed
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
