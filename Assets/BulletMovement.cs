389e32bb577972644aed1af04904153b786b023b
Aron Maijen <aron@mayen.at>
2024-03-07 12:15:04 +0100

    last commit

--- Assets/BulletMovement.cs
+++ Assets/BulletMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,7 +12,7 @@ public class BulletMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
@@ -29,7 +30,7 @@ public class BulletMovement : MonoBehaviour
         {
             SceneManager.LoadScene( SceneManager.GetActiveScene().name );
         }
-        
-        Destroy(this);
     }
+    
+    
 }
